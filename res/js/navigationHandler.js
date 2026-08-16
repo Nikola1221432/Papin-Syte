13ad27c8a98b3fabcf89e586d81c1dd6838a8a70
Nikola <nikolagg191@gmail.com>
2026-08-16 15:52:20 +0300

    style_update

--- res/js/navigationHandler.js
+++ res/js/navigationHandler.js
@@ -1,20 +1,5 @@
 function activatePageNavigation() {
   const navLinks = document.querySelectorAll(".nav-link");
-  const dropdownLinks = document.querySelectorAll(".dropdown-content a");
-  const dropdown = document.querySelector(".dropdown");
-
-  dropdownLinks.forEach((link) => {
-    link.addEventListener("click", (e) => {
-      e.preventDefault();
-      document
-        .querySelectorAll(".nav-link.active-page-indicator")
-        .forEach((el) => {
-          el.classList.remove("active-page-indicator");
-        });
-      link.classList.add("active-page-indicator");
-      window.location.href = link.href;
-    });
-  });
 
   navLinks.forEach((link) => {
     link.addEventListener("click", (e) => {
@@ -45,13 +30,12 @@ window.addEventListener("DOMContentLoaded", () => {
       ?.classList.add("active-page-indicator");
   } else if (currentPage === "history.html") {
     document
-      .querySelector('.dropdown-content a[href="history.html"]')
+      .querySelector('.nav-link[href="history.html"]')
       ?.classList.add("active-page-indicator");
   } else if (currentPage === "relics.html") {
     document
-      .querySelector('.dropdown-content a[href="relics.html"]')
+      .querySelector('.nav-link[href="relics.html"]')
       ?.classList.add("active-page-indicator");
   }
-
   activatePageNavigation();
 });
