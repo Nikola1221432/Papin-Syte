0b577401429961f76474805a5eb00cd542e09e3a
Nikola <nikolagg191@gmail.com>
2026-09-22 19:35:29 +0300

    update

--- server/server.js
+++ server/server.js
@@ -9,19 +9,6 @@ const MAX_NAMES = 10;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "res")));
 
-// Красивые URL без .html
-app.use((req, res, next) => {
-  // Если это GET-запрос и в пути нет точки (то есть это не файл типа .css или .png)
-  if (req.method === "GET" && !req.path.includes(".")) {
-    const filePath = path.join(__dirname, "..", "res", req.path + ".html");
-    res.sendFile(filePath, (err) => {
-      if (err) next(); // Если файла нет, идём дальше (например, к API или 404)
-    });
-  } else {
-    next();
-  }
-});
-
 function handleNotesSubmit(table) {
   return (req, res) => {
     const rawNames = Array.isArray(req.body?.names) ? req.body.names : [];
